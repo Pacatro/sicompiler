1fdec907a8ce933afdf3ec5e96c47b7540485e70
Pacatro <paco.algar.15@gmail.com>
2024-01-04 18:09:40 +0100

    validator started

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod tokenizer;
+mod validator;
 
 use tokenizer::Tokenizer;
+use validator::Validator;
 use std::env;
 
 fn main() {
@@ -14,14 +16,24 @@ fn main() {
     }
 
     let input_file: String = args[1].clone();
-    // let output_file: String = args[2].clone();
+    let output_file: String = args[2].clone();
+
+    // let input_file: String = "examples/input.txt".to_string();
+    // let output_file: String = "examples/out.txt".to_string();
 
     let tokenizer: Tokenizer = Tokenizer::new(input_file);
 
+    let mut tokens: Vec<String> = Vec::new();
+
     match tokenizer.tokenize() {
-        Ok(tokens) => println!("{:?}", tokens),
+        Ok(result) => tokens = result,
         Err(error) => eprintln!("Error: {error}")
     };
 
-    //  TODO: Implements VALIDATOR struct.
+    let validator: Validator = Validator::new(tokens, output_file);
+
+    match validator.validate() {
+        Ok(_) => println!("Success"),
+        Err(error) => eprintln!("Error: {error}")
+    };
 }
---
+++ src/validator.rs
@@ -0,0 +1,30 @@
+use std::fs::{File, OpenOptions};
+use std::io::{Write, Error};
+
+pub struct Validator {
+    output_file: String,
+    tokens: Vec<String>
+}
+
+impl Validator {
+    fn write_file(&self) -> Result<File, Error> {
+        let mut file: File = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open(&self.output_file)?;
+
+        for token in &self.tokens {
+            writeln!(file, "{}", token)?;
+        }
+
+        Ok(file)
+    }
+    
+    pub fn new(tokens: Vec<String>, output_file: String) -> Validator { Validator { tokens, output_file } }
+
+    // TODO: PENSAR ESTO MEJOR
+    pub fn validate(&self) -> Result<File, Error> {
+        let file: File = self.write_file()?;
+        Ok(file)
+    }
+}
